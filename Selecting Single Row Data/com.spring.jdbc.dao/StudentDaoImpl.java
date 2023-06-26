1e9e0fe636c2e450d7055f44bbb0b98011219315
Sahil Sartaj <46869388+sssahilsartaj@users.noreply.github.com>
2023-06-26 20:49:38 +0530

    StudentDaoImpl.java

---
+++ Selecting Single Row Data/com.spring.jdbc.dao/StudentDaoImpl.java
@@ -0,0 +1,29 @@
+package com.spring.jdbc.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.spring.jdbc.entities.Student;
+
+public class StudentDaoImpl implements StudentDao{
+	
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public Student getStudent(int studentId) {
+		// Select query
+		String query = "select * from student where id=?";
+		RowMapper<Student> rowMapper = new RowMapperImpl();
+		Student student = this.jdbcTemplate.queryForObject(query, rowMapper, studentId);
+		return student;
+	}
+	
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+}
