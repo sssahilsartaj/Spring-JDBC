a9aca66c3074015ab4d1a148412533ebde89b9da
Sahil Sartaj <46869388+sssahilsartaj@users.noreply.github.com>
2023-06-26 20:44:23 +0530

    StudentDaoImpl.java

---
+++ Deleting Data/com.spring.jdbc.dao/StudentDaoImpl.java
@@ -0,0 +1,28 @@
+package com.spring.jdbc.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.spring.jdbc.entities.Student;
+
+public class StudentDaoImpl implements StudentDao{
+	
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public int delete(int studentId) {
+		// delete query
+		String query = "delete from student where id=?";
+		int delete = this.jdbcTemplate.update(query, studentId);
+		return delete;
+	}
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+}
