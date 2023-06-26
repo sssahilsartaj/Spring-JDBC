b9203f63e89be2fa767dbb7b17b57700ccf037c2
Sahil Sartaj <46869388+sssahilsartaj@users.noreply.github.com>
2023-06-26 20:39:00 +0530

    StudentDaoImpl.java

---
+++ Updating Data/com.spring.jdbc.dao/StudentDaoImpl.java
@@ -0,0 +1,28 @@
+package com.spring.jdbc.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.spring.jdbc.entities.Student;
+
+public class StudentDaoImpl implements StudentDao{
+	
+	private JdbcTemplate jdbcTemplate;
+  
+  @Override
+	public int update(Student student) {
+		// update query
+		String query = "update student set name=?, city=? where id=?";
+		int update = this.jdbcTemplate.update(query, student.getName(), student.getCity(), student.getId());
+		return update;
+	}
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+}
